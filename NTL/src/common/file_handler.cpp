444e878901da560f84e45ff28ce30c246fc9a471
Omar Al-absi <omarabsi770@gmail.com>
2025-12-09 22:36:56 +0300

    added support for WPD

--- NTL/src/common/file_handler.cpp
+++ NTL/src/common/file_handler.cpp
@@ -47,6 +47,7 @@ namespace NTL::fh
             {"e_r", ntl.get_er()},
             {"d", ntl.get_d()},
             {"Cn", ntl.get_Cn()},
+            {"M", ntl.get_M()},
             {"readme", readme}
         };
     }
@@ -56,7 +57,55 @@ namespace NTL::fh
         if (j.at("json_type") != "NTL")
             throw(std::logic_error("Attempted to read an NTL from a different json object"));
 
-        return NTL(j.at("Z0"), j.at("e_r"), j.at("d"), j.at("Cn"));
+        return NTL(j.at("Z0"), j.at("e_r"), j.at("d"), j.at("Cn"), j.at("M"));
+    }
+
+    json wpd_to_json(const WPD::WPD& wpd, const std::string& readme)
+    {
+        if (wpd.get_ntl2().get_Cn().empty() || wpd.get_ntl3().get_Cn().empty() || !wpd.get_R())
+            throw(std::logic_error("Attempted to write an empty NTL into a json object"));
+
+        json j1 = {
+            {"json_type", "WPD"},
+            {"Z0", wpd.get_Z0()},
+            {"e_r", wpd.get_er()},
+            {"R", wpd.get_R()},
+            {"readme", readme}
+        };
+
+        json j2 = ntl_to_json(wpd.get_ntl2()); 
+        j2.erase("json_type");
+        j2.erase("readme");
+        j2.erase("Z0");
+        j2.erase("e_r");        
+        rename_json_element<double>(j2, "d", "d-2");
+        rename_json_element<std::vector<double>>(j2, "Cn", "Cn2");
+        rename_json_element<double>(j2, "M", "M-2");
+
+        json j3 = ntl_to_json(wpd.get_ntl3()); 
+        j3.erase("json_type");
+        j3.erase("readme");
+        j3.erase("Z0");
+        j3.erase("e_r");
+        rename_json_element<double>(j3, "d", "d-3");
+        rename_json_element<std::vector<double>>(j3, "Cn", "Cn3");
+        rename_json_element<double>(j3, "M", "M-3");
+
+        json j_total = j1;         
+        j_total.merge_patch(j2);   
+        j_total.merge_patch(j3);
+
+        return j_total;       
+    }
+
+    WPD::WPD json_to_wpd(const json& j)
+    {
+        if (j.at("json_type") != "WPD")
+            throw(std::logic_error("Attempted to read a WPD from a different json object"));
+
+        return WPD::WPD(NTL(j.at("Z0"), j.at("e_r"), j.at("d-2"), j.at("Cn2"), j.at("M-2")),
+            NTL(j.at("Z0"), j.at("e_r"), j.at("d-3"), j.at("Cn3"), j.at("M-3")),
+            j.at("R"));
     }
 
     std::fstream ntl_to_file(const NTL& ntl, const std::string& name, const std::string& readme)
@@ -67,5 +116,14 @@ namespace NTL::fh
     NTL file_to_ntl(const std::string& name)
     {
         return json_to_ntl(file_to_json(name));
+    }
+
+    std::fstream wpd_to_file(const WPD::WPD& wpd, const std::string& name, const std::string& readme)
+    {
+        return json_to_file(wpd_to_json(wpd, readme), name);
+    }
+    WPD::WPD file_to_wpd(const std::string& name)
+    {
+        return json_to_wpd(file_to_json(name));
     }
 }
--- NTL/src/common/file_handler.h
+++ NTL/src/common/file_handler.h
@@ -4,6 +4,7 @@
 #include <nlohmann/json.hpp>
 #include <fstream>
 #include "models/ntl.h"
+#include "models/wpd.h"
 #include "simulation/NTL_sim.h"
 #include "optimisation/NTL_opt.h"
 
@@ -11,7 +12,7 @@
 namespace NTL::fh
 {
 	using json = nlohmann::json;
-
+	
 	//json to file
 	std::fstream json_to_file(const json& j, const std::string& name);
 	json file_to_json(const std::string& name);
@@ -19,6 +20,10 @@ namespace NTL::fh
 	//NTL to json
 	json ntl_to_json(const NTL& ntl, const std::string& readme = std::string());
 	NTL json_to_ntl(const json& j);
+
+	//WPD to json
+	json wpd_to_json(const WPD::WPD& ntl, const std::string& readme = std::string());
+	WPD::WPD json_to_wpd(const json& j);
 
 	//setup to json
 	template<class T>
@@ -38,18 +43,30 @@ namespace NTL::fh
 	std::fstream ntl_to_file(const NTL& ntl, const std::string& name, const std::string& readme = std::string());
 	NTL file_to_ntl(const std::string& name);
 
+	//WPD to file
+	std::fstream wpd_to_file(const WPD::WPD& wpd, const std::string& name, const std::string& readme = std::string());
+	WPD::WPD file_to_wpd(const std::string& name);
+
 	//setup to file
-	template<class T>
+	template<typename T>
 	std::fstream setup_to_file(const T& setup, const std::string& name, const std::string& readme = std::string())
 	{
 		return json_to_file(setup_to_json<T>(setup, readme), name);
 	}
 
-	template<class T>
+	template<typename T>
 	T file_to_setup(const std::string& name)
 	{
 		return json_to_setup<T>(file_to_json(name));
 	}
 
+	//Helpers
+	template<typename T>
+	void rename_json_element(json& j, const std::string& old_name, const std::string& new_name)
+	{
+		T val = j.at(old_name).get<T>();
+		j.erase(old_name);
+		j[new_name] = val;
+	}
 
 }
