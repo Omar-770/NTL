eda006fc070a1982dcf4e97cbb3c8e5332a93fa3
Omar Al-absi <omarabsi770@gmail.com>
2026-03-09 21:58:51 +0300

    Specific WPD simulation tool

---
+++ WPD_simulator/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <vector>
+#include <array>
+#include <complex>
+#include <algorithm>
+#include "qt_plot.h"
+#include "models/ntl.h"
+#include "models/wpd.h"
+#include "simulation/NTL_sim.h"
+#include "simulation/WPD_sim.h"
+#include "common/file_handler.h"
+#include "common/enums.h"
+#include "common/helpers.h"
+
+namespace fh = NTL::fh;
+
+int main(int argc, char* argv[])
+{
+	try
+	{
+		QApplication app(argc, argv);
+
+		std::string folder_name;
+		std::cout << "Folder Name: ";
+		std::cin >> folder_name;
+		std::cout << "\n\n\n";
+
+		//build
+
+		NTL::NTL ntl2 = fh::file_to_ntl(folder_name + "/" + folder_name +"_ntl2");
+		NTL::NTL ntl3 = fh::file_to_ntl(folder_name + "/" + folder_name + "_ntl3");
+		NTL::NTL out2 = fh::file_to_ntl(folder_name + "/" + folder_name + "_out2");
+		NTL::NTL out3 = fh::file_to_ntl(folder_name + "/" + folder_name + "_out3");
+		auto R = fh::file_to_json(folder_name + "/" + folder_name + "_wpd").at("R");
+		auto setup = fh::file_to_setup<WPD::opt_setup>(folder_name + "/" + folder_name + "_setup");
+
+		WPD::WPD wpd(ntl2, ntl3, R);
+
+		double f_min = 1e7;
+		double f_max = *std::max_element(setup.freqs.cbegin(), setup.freqs.cend());
+		double f_step = 1e6;
+
+		//simulate 		
+		NTL::sim nsim;
+		WPD::sim wsim(f_min, 1.2 * f_max, f_step, setup.freqs);
+
+		wsim.add_window(nsim.w_h_profile(ntl2, "NTL2"));
+		wsim.add_window(nsim.z_profile(ntl2, "NTL2"));
+
+		wsim.add_window(nsim.w_h_profile(ntl3, "NTL3"));
+		wsim.add_window(nsim.z_profile(ntl3, "NTL3"));
+
+		wsim.add_window(nsim.w_h_profile(out2, "Out2"));
+		wsim.add_window(nsim.z_profile(out2, "Out2"));
+
+		wsim.add_window(nsim.w_h_profile(out3, "Out3"));
+		wsim.add_window(nsim.z_profile(out3, "Out3"));
+
+		wsim.sparams(wpd, setup.Zref, out2, out3);
+
+		wsim.merge("WPD");
+		
+		app.exec();		
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "\nCRITICAL ERROR: " << e.what() << std::endl;
+		std::cerr << "Press Enter to exit..." << std::endl;
+		std::cin.get();
+	}
+
+	return 0;
+}
